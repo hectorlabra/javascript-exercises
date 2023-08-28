42b01dfc2175cfff3e4e6814c360e4b8b65fa45e
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-08-28 19:37:30 -0400

    04 finish

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...args) {
+  let filteredArray = array.filter((item) => !args.includes(item));
 
-};
+  return filteredArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
