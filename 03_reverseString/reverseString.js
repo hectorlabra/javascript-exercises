daa4ac032d344d59e663033032c0048e4f27e6c4
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-08-28 19:29:08 -0400

    03 finish

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+function reverseString(inputString) {
+  let characters = inputString.split("");
+  let reversedCharacters = characters.reverse();
+  let reversedString = reversedCharacters.join("");
+  return reversedString;
+}
+console.log(reverseString("hello there")); // Debería imprimir 'ereht olleh'
 
 // Do not edit below this line
 module.exports = reverseString;
