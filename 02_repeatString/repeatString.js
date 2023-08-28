9b0bd78a7e565813b8ec56cf335b7f7d8848357f
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-08-28 19:22:38 -0400

    02 finish

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+function repeatString(string, num) {
+  if (num < 0) {
+    return 'ERROR';
+  }
 
-};
+  let result = ''; 
+  
+  for (let i = 0; i < num; i++) {
+    result += string; 
+  }
+  
+  return result; 
+}
 
 // Do not edit below this line
 module.exports = repeatString;
