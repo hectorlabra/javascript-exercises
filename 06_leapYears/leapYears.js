2864333c1c2c1e2a0b494c72a167bccefed4144f
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-08-31 17:33:08 -0400

    06 finish

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
-
-};
+function leapYears(year) {
+  if (year % 4 === 0) {
+    if (year % 100 !== 0 || year % 400 === 0) {
+      return true;
+    }
+  }
+  return false;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
