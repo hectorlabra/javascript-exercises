261d08fae90a199b0c6aa608c079c21d5a4e8290
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-08-31 17:28:36 -0400

    05 finish

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,10 @@
 function removeFromArray(array, ...args) {
-  let filteredArray = array.filter((item) => !args.includes(item));
-
+  let filteredArray = array.filter(item => !args.includes(item));
   return filteredArray;
 }
+
+console.log(removeFromArray([1, 2, 3, 4], 3)); // Debería imprimir [1, 2, 4]
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+function sumAll(num1, num2) {
+  // Verifica si alguno de los argumentos no es un número
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  }
 
-};
+  // Verifica si alguno de los argumentos es negativo
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
 
-// Do not edit below this line
-module.exports = sumAll;
+  // Verifica si num1 es mayor que num2 y, si es así, intercambia los valores
+  if (num1 > num2) {
+    [num1, num2] = [num2, num1];
+  }
+
+  let sum = 0;
+  for (let i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+module.exports = sumAll; // Exporta la función para que pueda ser usada en los tests
